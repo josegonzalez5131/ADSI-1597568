7e7e7a0e77f4a20cf1d9e4c12305c0845934eb55
Daniela <Daniela@192.168.1.19>
2018-05-02 20:19:28 -0500

    my proyect PHP

---
+++ Planeacion/ejercicios1-37/point10.php
@@ -0,0 +1,7 @@
+<?php
+define("CONSTANTE", "definition of the constant.",true);
+echo CONSTANTE; // muestra "definition of the constant."
+echo Constante; // muestra "Constant" A constant is an identifier (name) for a simple value. .
+
+
+?>
